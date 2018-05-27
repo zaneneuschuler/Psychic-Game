abc7da68eaa6d1f0dbb122501f7387695b86e440
zane <neuschuler@gmail.com>
2018-05-26 23:59:26 -0700

    i'm tired

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    //init wins losses and guesses
+    $('.wins').html("<h4>Wins:");
+    $('.losses').html("<h4>Losses:");
+    $('.guesses').html("<h4>Your guesses:");
+    //init guesses array
+    let computerGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+});
